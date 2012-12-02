656d61ccfda7dd205d718ace724798100a5b768f
L0C0 <zadr.true@ya.ru>
2012-12-02 23:03:05 +0600

    Created classes
    Created classes for arguments and options.
    Probably they will be refactored soon.

---
+++ Execut0r/Argument.cs
@@ -0,0 +1,19 @@
+﻿namespace Execut0r
+{
+    public class Argument
+    {
+        public Argument()
+        {
+        }
+
+        public Argument(string name, string parameter)
+        {
+            Name = name;
+            Parameter = parameter;
+        }
+
+        public string Name { get; set; }
+
+        public string Parameter { get; set; }
+    }
+}
---
+++ Execut0r/Option.cs
@@ -0,0 +1,16 @@
+﻿namespace Execut0r
+{
+    public class Option
+    {
+        public Option()
+        {
+        }
+
+        public Option(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
