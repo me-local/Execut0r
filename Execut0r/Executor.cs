49085fd069c0178b76f807a1f861ffd6add7cfc3
L0C0 <zadr.true@ya.ru>
2012-12-02 23:39:54 +0600

    First working version
    It works, but useless for now

---
+++ Execut0r/Executor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Execut0r
+{
+    public class Executor
+    {
+        public Executor()
+        {
+        }
+
+        public Executor(string path, List<Option> options, List<Argument> arguments)
+        {
+            Path = path;
+            Options = options;
+            Arguments = arguments;
+        }
+
+        public string Path;
+        public List<Option> Options { get; set; }
+        public List<Argument> Arguments { get; set; }
+
+        public void Execute()
+        {
+            var command = new StringBuilder();
+
+            foreach (var option in Options) //Options
+            {
+                command.Append(" ");
+                command.Append(option.Name);
+            }
+
+            foreach (var argument in Arguments) //Arguments
+            {
+                command.Append(" ");
+                command.Append(argument.Name);
+                command.Append(" ");
+                command.Append(argument.Parameter);
+            }
+
+            Process.Start(Path, command.ToString());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Execut0r is library created for easy interaction with external programs
 
 TODO
 ===============
+* Command string overriding
+* More options
+* Example app
+* Program output (interaction, not only starting)
 * Async
 * Tests
 * Advanced documentation
